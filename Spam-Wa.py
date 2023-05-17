a92c8db3cc3c80a51b5905a4626dae3ecac9dc47
[ Dvanmeploph - ( Ferly ) ] - <122451961+Dvanmeploph@users.noreply.github.com>
2023-05-17 15:00:04 +0700

    Add files via upload

---
+++ Spam-Wa.py
@@ -0,0 +1,10 @@
+import time
+import random
+import pyautogui as pg
+
+time.sleep(5)
+pesan_random = ["Ngentot", "Woi Bangsat", "Anjing", "Tolol", "Woy Kontol"]
+for i in range(100):
+    pg.typewrite(random.choice(pesan_random))
+    pg.press("enetr")
+    time.sleep(0.5)
